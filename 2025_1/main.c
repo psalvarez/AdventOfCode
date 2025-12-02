2f5a3025a21e8b0edd25e4b10d07dcf8961a8aee
Pedro Sanchez <pedro.sanchez@foundry.com>
2025-12-02 10:37:14 +0000

    2025-1 Part2

--- 2025_1/main.c
+++ 2025_1/main.c
@@ -4,12 +4,11 @@
 
 #define DIAL_MAX 100 // Maximum number is 99
 
-int nextNumber(int in, char* inst)
+int nextNumber(int in, char* inst, int* clicksOnZero)
 {
     // Parse string
     char* numberStr = inst + 1;
     int delta = atoi(numberStr);
-    printf("Delta %d\n", delta);
     int mult = 1;
     if (*inst == 'L')
     {
@@ -19,11 +18,35 @@ int nextNumber(int in, char* inst)
     {
         printf("Wrong read\n");
     }
+    printf("Delta %d\n", mult * delta);
 
     // Next number
     int out = in + mult * delta;
     printf("Bare out %d\n", out);
-    
+
+    // Part 2. I couldn't be bothered to recreate the whole math, so just went with a 1 by 1 counter.
+    int secondOut = in;
+    for (int i = delta; i > 0; --i)
+    {
+        secondOut += mult;
+
+        if (secondOut == DIAL_MAX)
+        {
+            secondOut = 0;
+        }
+        else if (secondOut < 0)
+        {
+            secondOut = 99;
+        }
+
+        if (secondOut == 0)
+        {
+            ++(*clicksOnZero);
+        }
+    }
+    printf("SecondOut %d, clicksOnZero %d\n", secondOut, *clicksOnZero);
+
+    // Part 1
     while (out < 0 || out >= DIAL_MAX)
     {
         if (out < 0)
@@ -35,6 +58,7 @@ int nextNumber(int in, char* inst)
             out = out - DIAL_MAX;
         }
     }
+
     return out;
 }
 
@@ -59,10 +83,13 @@ int main(int argc, char* argv[])
     char line[10];
     int dial = 50;
     int password = 0;
+    int clicksOnZero = 0; // Part 2
     while (fgets(line, sizeof(line), inFile) != NULL)
     {
-        dial = nextNumber(dial, line);
-        printf("%d -> %s\n", dial, line);
+        printf("%s", line);
+        int prevDial = dial;
+        dial = nextNumber(dial, line, &clicksOnZero);
+        printf("%d -> %d\n\n", prevDial, dial);
 
         if (dial == 0)
         {
@@ -72,6 +99,7 @@ int main(int argc, char* argv[])
     printf("\n");
 
     printf("Password is %d\n", password);
+    printf("Password part 2 is %d\n", clicksOnZero);
 
     fclose(inFile);
 }
